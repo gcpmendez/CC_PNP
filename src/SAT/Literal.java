4245888cbe41542c0b6d006059a9d2de98f329b3
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-11 12:12:43 +0000

    Siguiendo con el solver, metodos de Literal.

--- src/SAT/Literal.java
+++ src/SAT/Literal.java
@@ -1,9 +1,32 @@
 package SAT;
 
 public class Literal {
+    // Nombre del literal
 	private String name;
+	// Si el literal esta negado
 	private boolean negated;
+	// Valor que esta tomando (antes de ser negada o no)
+	private boolean rawValue;
 	
+	public Literal(String name, boolean negated, boolean rawValue){
+	    this.name = name;
+	    this.negated = negated;
+	    this.rawValue = rawValue;
+	}
+	
+	/**
+	 * Si al crear no se pasa valor inicial, se asume true.
+	 */
+	public Literal(String name, boolean negated){
+        this(name, negated, true);
+    }
+	
+	/**
+	 * Constructor de copia.
+	 */
+	public Literal(Literal other){
+	    this(other.name, other.negated, other.rawValue);
+	}
 	
 	public String toString () {
 		String str = "";
@@ -14,4 +37,28 @@ public class Literal {
 		}
 		return str;
 	}
+	
+	/*
+	 * Getters and setters.
+	 */
+
+    public boolean isNegated() {
+        return negated;
+    }
+
+    public void setNegated(boolean negated) {
+        this.negated = negated;
+    }
+
+    public boolean isRawValue() {
+        return rawValue;
+    }
+
+    public void setRawValue(boolean rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/SOLVER/SAT_Solver.java
+++ src/SOLVER/SAT_Solver.java
@@ -2,18 +2,30 @@ package SOLVER;
 
 import java.util.ArrayList;
 
+import SAT.Literal;
 import SAT.SATProblemInstance;
 
 public abstract class SAT_Solver {
     
     static public boolean isSatisfiable(SATProblemInstance prob){
         
-        // Numero de literales.
+        // Numero total de literales en la instancia.
         int literalsNum = prob.getLiterals().size();
         
-        // 
-        ArrayList<Literal> lastLiteralConfiguration = new ArrayList<Literal>();
+        // Primera configuracion posible, que cambiaremos para ir probando
+        ArrayList<Literal> literalConfiguration = new ArrayList<Literal>();
         
-        for ()
+        // Construimos la configuracion inicial y la final de variables
+        for (int i =0; i < literalsNum; i++){
+            literalConfiguration.add(new Literal(prob.getLiterals().get(i)));
+        }
+        
+        // Para cada configuracion posible de las variables...
+        boolean stop = false;
+        while(!stop){
+            break;  // TODO: esto pa que german no se queje xd
+        }
+        
+        return false;
     }
 }
