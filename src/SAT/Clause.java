c4d08c04abdfc3ae61693faf6362d2f5e993cbda
Germán Méndez <gcpmendez@gmail.com>
2015-12-11 11:30:13 +0000

    Updating SAT

---
+++ src/SAT/Clause.java
@@ -0,0 +1,17 @@
+package SAT;
+
+import java.util.ArrayList;
+
+public class Clause {
+	private ArrayList<Literals> literalsSet;
+	
+	
+	
+	
+	public void to_s () {
+		for (int i = 0; i < literalsSet.size(); i++) {
+			
+			
+		}
+	}
+}
