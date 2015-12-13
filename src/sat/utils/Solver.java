26c0d367b6bb205e881a32537e19ec2133290a45
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-13 17:57:57 +0000

    Solver. Sin bucle pero la traza la hace mal.

--- src/sat/utils/Solver.java
+++ src/sat/utils/Solver.java
@@ -19,8 +19,14 @@ public abstract class Solver {
         
         PossibleConfigs possibleConfigs = new PossibleConfigs(literalNames);
         for (Config c : possibleConfigs){
-            tryClauses(prob.getClauses(), c);
+            // Con tal de que una configuracion satisfaga el problema, el problema es satisfactible.
+            // (Esto es porque en una problema SAT sólo se necesita una configuración de valores válida).
+            if (tryClauses(prob.getClauses(), c) == true){
+                Sys.fout("%s: La configuración vale! El problema ES SATISFACTIBLE.", c);
+                return true;
+            };
         }
+        Sys.fout("Ninguna configuración vale! El problema NO ES SATISFACTIBLE.");
         return false; // Ninguna de las configuraciones devuelve true: el problema no es satisfactible.
     }
     
@@ -33,11 +39,12 @@ public abstract class Solver {
         for(Clause c : clauses){
             // Con tal de que un literal sea falso, la condicion no se cumple.
             // (Esto es porque en una problema SAT todas las clausulas estan unidas mediante AND).
-            if (trySingleClause(c, config) == true){
-                Sys.fout("%s", config);
+            if (trySingleClause(c, config) == false){
+                Sys.fout("%s no cumple! Esta configuración no satisface las cláusulas.", config);
                 return false; 
             }
         }
+        Sys.fout("%s cumple! Esta configuración satisface el problema. El problema es satisfactible.", config);
         return true; // Todas las clausulas devuelven true: la configuracion satisface el problema.
     }
     
