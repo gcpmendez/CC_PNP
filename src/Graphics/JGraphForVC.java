7bb29f7121f0b0ee161460a67ae41559fd9fe90a
Germán Méndez <gcpmendez@gmail.com>
2015-12-15 18:14:50 +0000

    Adding Graphs for VC

---
+++ src/Graphics/JGraphForVC.java
@@ -0,0 +1,47 @@
+package Graphics;
+
+import javax.swing.JFrame;
+
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+
+public class JGraphForVC extends JFrame
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2707712944901661771L;
+
+	public JGraphForVC()
+	{
+		super("Hello, World!");
+
+		mxGraph graph = new mxGraph();
+		Object parent = graph.getDefaultParent();
+
+		graph.getModel().beginUpdate();
+		try
+		{
+			Object u1 = graph.insertVertex(parent, null, "u1", 20, 20, 80, 30);
+			Object u2 = graph.insertVertex(parent, null, "u1!", 240, 150, 80, 30);
+			graph.insertEdge(parent, null, "Edge", u1, u2);
+		}
+		finally
+		{
+			graph.getModel().endUpdate();
+		}
+
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		getContentPane().add(graphComponent);
+	}
+
+	public static void main(String[] args)
+	{
+		JGraphForVC frame = new JGraphForVC();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 320);
+		frame.setVisible(true);
+	}
+
+}
--- src/objects/VertexCover.java
+++ src/objects/VertexCover.java
@@ -1,5 +1,16 @@
 package objects;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class VertexCover {
+	private ArrayList<String> V;
+	private HashMap<String, String> E;
+	
+	public VertexCover () {
+		V = new ArrayList<String>();
+		E = new HashMap<String, String>();
+	}
+	
 	
 }
